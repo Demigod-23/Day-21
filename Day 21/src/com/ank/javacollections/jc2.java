c8e809a4e95cb41afda1f421b9f69fe4d6981b3d
AnK <AnK@DESKTOP-35CEPU1>
2022-08-12 01:03:10 +0530

    day 21 code

---
+++ Day 21/src/com/ank/javacollections/jc2.java
@@ -0,0 +1,27 @@
+package com.ank.javacollections;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+class jc2
+
+{
+
+public static void main(String [] args)
+
+{
+
+Collection<Integer> list = new ArrayList();
+
+list.add(100);
+
+list.add(10);
+
+list.add(1000);
+
+
+System.out.println("ArrayList : " + list);
+
+}
+
+}
---
+++ Day 21/src/com/ank/javacollections/jc3.java
@@ -0,0 +1,32 @@
+package com.ank.javacollections;
+import java.util.*;
+
+public class jc3
+
+{
+
+public static void main(String args[])
+
+{
+
+List <Integer> l = new ArrayList<Integer>();
+
+l.add(1);
+
+l.add(2);
+
+l.add(3);
+
+l.add(4);
+
+l.add(5);
+
+l.add(6);
+
+l.add(4);
+
+System.out.println(l);
+
+}
+
+}
